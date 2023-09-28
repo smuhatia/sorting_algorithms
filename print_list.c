b65fe6f2321f64529903f117ef86c8a2d026a39d
wonder25 <wonderfulmwinzi14@gmail.com>
2023-09-28 12:20:48 +0300

    print list

---
+++ print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * print_list - prints list of integers
+ *
+ * @list: list to be printed
+ */
+
+void print_list(const listint_t *list)
+{
+	int j;
+
+	j = 0;
+	while (list)
+	{
+		if (j > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++j;
+		list = list->next;
+	}
+	printf("\n");
+}
